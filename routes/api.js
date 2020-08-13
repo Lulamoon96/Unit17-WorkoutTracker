ee940ba828e9c3c38a44a5d04434387bd6d5f6f7
Kuroneko-Rares <luigi.galvan@berkeley.edu>
2020-08-12 20:19:25 -0700

    Kinda done?

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+const router = require("express").Router()
+const Workout = require("../models/Workout")
+
+router.post("/api/workouts", (req, res) => {
+
+    Workout.create(req.body)
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => res.json(err))
+
+})
+
+router.put("/api/workouts/:id", (req, res) => {
+
+    Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body}})
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => res.json(err))
+
+})
+
+router.get("/api/workouts", (req, res) => {
+
+    Workout.find()
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => res.json(err))
+
+})
+
+router.get("/api/workouts/range", (req, res) => {
+
+    Workout.find()
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => res.json(err))
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -17,48 +15,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
-app.get("/notes", (req, res) => {
-  db.Note.find({})
-    .then(dbNote => {
-      res.json(dbNote);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  db.User.find({})
-    .populate("notes")
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+app.use(require("./routes/api.js"))
+app.use(require("./routes/views.js"))
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`)
 });
